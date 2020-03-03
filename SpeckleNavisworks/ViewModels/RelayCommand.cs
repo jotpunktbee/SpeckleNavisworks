1f5f3195a485ad4cc89e858c720de45b06eb2c9e
Julian Bolliger <julian.bolliger@mum.ch>
2020-03-03 22:36:52 +0100

    Added RelayCommand for MVVM

---
+++ SpeckleNavisworks/ViewModels/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SpeckleNavisworks.ViewModels
+{
+    // https://stackoverflow.com/questions/1468791/icommand-mvvm-implementation
+    // https://msdn.microsoft.com/en-us/magazine/dd419663.aspx
+    public class RelayCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
