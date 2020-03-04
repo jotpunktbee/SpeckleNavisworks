f1708b454cda52ef2bc068bf22bceeffb8a42ebc
Julian Bolliger <julian.bolliger@mum.ch>
2020-03-04 22:04:37 +0100

    Aligned UI to existing Revit plugin

---
+++ SpeckleNavisworks/ViewModels/NewStream.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpeckleCore;
+
+namespace SpeckleNavisworks.ViewModels
+{
+    public class NewStream : Base
+    {
+        private SpeckleApiClient _client;
+
+        public SpeckleApiClient Client
+        {
+            get
+            {
+                return _client;
+            }
+            set
+            {
+                _client = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NewStream()
+        {
+        }
+
+        public NewStream(SpeckleApiClient client)
+        {
+            Client = client;
+        }
+    }
+}
